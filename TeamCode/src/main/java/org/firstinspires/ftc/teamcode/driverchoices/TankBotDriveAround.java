4d5ee7044b216b11152be2119943a50e911b6df6
Makiah Bennett <makiahtbennett@gmail.com>
2017-03-28 15:36:42 -0400

    Update TankBotDriveAround with Anthony's changes
    Extra changes required...?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverchoices/TankBotDriveAround.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverchoices/TankBotDriveAround.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.driverchoices;
 
+import java.io.Console;
+import java.util.HashMap;
+
 import android.media.MediaPlayer;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,195 +14,154 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.ImprovedOpModeBase;
 import org.firstinspires.ftc.teamcode.debugging.ConsoleManager;
 
-//Add the teleop to the op mode register.
 @TeleOp(name="TankBot Drive", group="TankBot Group")
 
 public class TankBotDriveAround extends ImprovedOpModeBase
 {
-    /*** CONFIGURE ALL ROBOT ELEMENTS HERE ***/
-    //Drive motors (they are lists because it helps when we add on new motors.
-    private DcMotor leftMotor, rightMotor;
-    private Servo turret;
-    private double motorCorrectionFactor = -0.1;
+    private static final double MOTOR_CORRECTION_FACTOR = -0.1;
+    private static final double TRIGGER_MIN_POSITION = 0.5;
+    private static final double TURRET_SPEED = 0.005;
 
-    // Called on initialization (once)
+    private enum Motors
+    {
+        LEFT, RIGHT;
+    }
+
+    private Servo turret;
+    private DcMotor lMotor, rMotor;
+
+    /* Called on initialization (once) */
     protected void initializeHardware() throws InterruptedException
     {
-        //Make sure that the robot components are found and initialized correctly.
-        //This all happens during init()
-        /*************************** DRIVING MOTORS ***************************/
-        leftMotor = initialize(DcMotor.class, "Left Motor");
-        rightMotor = initialize(DcMotor.class, "Right Motor");
-        leftMotor.setDirection (DcMotorSimple.Direction.REVERSE);
-
         turret = initialize(Servo.class, "Turret");
+        lMotor = initialize(DcMotor.class, "Left Motor");
+        rMotor = initialize(DcMotor.class, "Right Motor");
+
+        lMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    private void setRightPower(double power)
-    {
-        rightMotor.setPower (power * (1 - motorCorrectionFactor));
-    }
-
-    private void setLeftPower(double power)
-    {
-        leftMotor.setPower (power * (1 + motorCorrectionFactor));
-    }
-
-    //All teleop controls are here.
+    /* Teleop controls. */
     protected void driverStationSaysGO() throws InterruptedException
     {
-        //Normal mode variables
-        double leftPower, rightPower;
+        double lPower, rPower;
         boolean backwards = false;
-        long lastTimeBackTogglePressed = System.currentTimeMillis(),
-                lastTimeMusicTogglePressed = System.currentTimeMillis ();
         double currentTurretPosition = 0.5;
 
-        //Keep looping while opmode is active (waiting a hardware cycle after all of this is completed, just like loop())
         while (opModeIsActive())
         {
-            /**************************** CONTROLLER #1 ********************************/
-            /************** Direction Toggle **************/
+            /* Going backwards? */
             if (!backwards)
-            { // Driving forward
-                leftPower = gamepad1.left_stick_y;
-                rightPower = gamepad1.right_stick_y;
-            } else { // Driving backward
-                leftPower = -gamepad1.right_stick_y;
-                rightPower = -gamepad1.left_stick_y;
+            {
+                lPower = gamepad1.left_stick_y;
+                rPower = gamepad1.right_stick_y;
+            } else
+            {
+                lPower = -gamepad1.right_stick_y;
+                rPower = -gamepad1.left_stick_y;
             }
 
-            /************** Motor Speed Control **************/
-            rightPower = Range.clip(rightPower, -1, 1);
-            leftPower = Range.clip(leftPower, -1, 1);
+            /* Control and set speed. */
+            setPower(scaleInput(Range.clip(lPower, -1, 1)), Motors.LEFT);
+            setPower(scaleInput(Range.clip(rPower, -1, 1)), Motors.RIGHT);
 
-            // Write the values to the motors.  Scale the robot in order to run the robot more effectively at slower speeds.
-            setLeftPower (scaleInput(leftPower));
-            setLeftPower (scaleInput(rightPower));
-
-            //Toggle direction
-            if (gamepad1.a && (System.currentTimeMillis() - lastTimeBackTogglePressed) > 500)
+            if(canPress("a"))
             {
                 backwards = !backwards;
-                lastTimeBackTogglePressed = System.currentTimeMillis();
             }
-
-            if (gamepad1.y && (System.currentTimeMillis () - lastTimeMusicTogglePressed) > 500)
+            if(canPress("y"))
             {
-                if (mediaPlayer != null)
-                    StopPlayingAudio ();
-                else
-                    PlayAudio (DownloadedSongs.IMPERIAL_MARCH);
+                MediaPlayerWrapper.playAudio(MediaPlayerWrapper.DownloadedSongs.IMPERIAL_MARCH);
             }
 
-            /************** Turret Position **************/
+            /* Turret position */
             if (turret != null)
             {
-                if (gamepad1.right_trigger > 0.5)
-                    currentTurretPosition -= 0.005;
-                else if (gamepad1.left_trigger > 0.5)
-                    currentTurretPosition += 0.005;
+                if (gamepad1.right_trigger > TRIGGER_MIN_POSITION)
+                {
+                    currentTurretPosition -= TURRET_SPEED;
+                }
+                else if (gamepad1.left_trigger > TRIGGER_MIN_POSITION)
+                {
+                    currentTurretPosition += TURRET_SPEED;
+                }
 
-                currentTurretPosition = Range.clip(currentTurretPosition, 0, 1);
-                turret.setPosition(currentTurretPosition);
+                turret.setPosition(Range.clip(currentTurretPosition, 0, 1));
             }
 
-            /************** Data Output **************/
-            ConsoleManager.outputConstantDataToDrivers(new String[] {
-                    "Right power = " + rightPower,
-                    "Music playing = " + (mediaPlayer != null)
-            });
-            /******************** END OF LOOP ********************/
+            /* Data output. */
+            ConsoleManager.outputConstantDataToDrivers(
+                new String[] {"Right power = " + rPower, "Music playing = " + (MediaPlayerWrapper.isPlaying())}
+            );
 
             idle();
-
-        }
-    }
-
-    /*
-     * This method scales the joystick input so for low joystick values, the
-     * scaled value is less than linear.  This is to make it easier to drive
-     * the robot more precisely at slower speeds.
-     */
-
-    double scaleInput(double dVal)
-    {
-        double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
-
-        // get the corresponding index for the scaleInput array.
-        int index = (int) (dVal * 16.0);
-        if (index < 0) {
-            index = -index;
-        } else if (index > 16) {
-            index = 16;
-        }
-
-        double dScale = 0.0;
-        if (dVal < 0) {
-            dScale = -scaleArray[index];
-        } else {
-            dScale = scaleArray[index];
-        }
-
-        return dScale;
-    }
-
-    /************ MEDIA PLAYER STUFF!!!!! ************/
-    protected enum DownloadedSongs
-    {
-        IMPERIAL_MARCH
-    }
-    private MediaPlayer mediaPlayer = null;
-    protected void PlayAudio(DownloadedSongs choice)
-    {
-        try
-        {
-            int selectedSong = com.qualcomm.ftcrobotcontroller.R.raw.imperialmarch;
-            switch (choice)
-            {
-                case IMPERIAL_MARCH:
-                    selectedSong = com.qualcomm.ftcrobotcontroller.R.raw.imperialmarch;
-                    break;
-            }
-            mediaPlayer = MediaPlayer.create(hardwareMap.appContext, selectedSong);
-            mediaPlayer.start();
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
-            {
-                public void onCompletion(MediaPlayer mediaPlayer1)
-                {
-                    mediaPlayer1.release();
-                }
-            });
-
-            ConsoleManager.outputNewLineToDrivers ("Playing " + choice.toString());
-
-            sleep (1000); //Give the MediaPlayer some time to initialize, and register that a song is being played.
-        }
-        catch (Exception e)
-        {
-            ConsoleManager.outputNewLineToDrivers ("Error while attempting to play music.");
-            return;
-        }
-    }
-
-    //Used to make the media player stop playing audio, and also to prevent excess memory allocation from being taken up.
-    protected void StopPlayingAudio()
-    {
-        if (mediaPlayer != null)
-        {
-            if (mediaPlayer.isPlaying())
-                mediaPlayer.stop(); //stop playing
-            mediaPlayer.release(); //prevent resource allocation
-            mediaPlayer = null; //nullify the reference.
         }
     }
 
     protected void driverStationSaysSTOP()
     {
-        StopPlayingAudio ();
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+        MediaPlayerWrapper.stopAudio();
+        setPower(0, Motors.LEFT);
+        setPower(0, Motors.RIGHT);
     }
 
+    protected void setPower(double power, Motors motorID)
+    {
+        (motorID.equals(Motors.LEFT) ? lMotor : rMotor).setPower(power);
+    }
+
+    /**
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    private double scaleInput(double dVal)
+    {
+        double[] scaleArr = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
+
+        /* Get corresponding index. */
+        int i = ((int)(dVal * 16.0)) % 16;
+        i = i < 0 ? -i : i;
+
+        return dVal < 0 ? -scaleArr[i] : scaleArr[i];
+    }
+
+    /**
+     * Checks if enough time has passed that a button can be pressed, and if
+     * that button is being pressed at all.
+     *
+     * Todo: A debounce may be more effective.
+     * Todo: Extrememly time expensive (use more variables).
+     */
+    private boolean canPress(String button) // Todo: Figure out the real class.
+    {
+        try
+        {
+            /* The game button and the last time it was pressed. */
+            final HashMap<String, Long> lastPressed = new HashMap<>(2); // Todo: Shouldn't be created more than once. May need to move to a field.
+            boolean beingPressed = (boolean)gamepad1.getClass().getDeclaredField(button).get(gamepad1); // TODO: Eww...
+
+            if(beingPressed)
+            {
+            /* As buttons are pressed, they are added to the map so their times can be tracked.*/
+                if(!lastPressed.containsKey(button)) // TODO: Probaby can be simplified with native methods.
+                {
+                    lastPressed.put(button, System.currentTimeMillis());
+                }
+
+                if((System.currentTimeMillis() - lastPressed.get(button)) > 500)
+                {
+                    lastPressed.put(button, System.currentTimeMillis())
+                    return true;
+                }
+                return false;
+            }
+        } catch (NoSuchFieldException e)
+        {
+            ConsoleManager.outputNewLineToDrivers("No such button found.");
+        } catch (IllegalAccessException e)
+        {
+            ConsoleManager.outputNewLineToDrivers("An error while attempting to get the button, I think.");
+        }
+    }
 }
